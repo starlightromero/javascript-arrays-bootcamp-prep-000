852ac92dbb907c057663ac08c9e92e1f55533f13
Starlight Romero <starlightromero@gmail.com>
2017-11-29 17:24:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,3 +29,11 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1)
 }
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  return array.pop()
+}
+
+function removeElementFromEndOfArray() {
+  
+}
