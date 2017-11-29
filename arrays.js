25a51baa4e85395cb25ea3b1f2ee76bdf2f63cdd
Starlight Romero <starlightromero@gmail.com>
2017-11-29 17:13:52 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,19 +1,19 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element) {
-  return [`${element}`, ...array];
+  return [element, ...array];
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(`${element}`);
+  array.unshift(element);
 }
 
 function addElementToEndOfArray(array, element) {
-  return [...array, `${element}`];
+  return [...array, element];
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-  array.push(`${element}`);
+  array.push(element);
 }
 
 function accessElementInArray(array, index) {
