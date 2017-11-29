1b7158e7750db915f214436c0f8ae60844fc1171
Starlight Romero <starlightromero@gmail.com>
2017-11-29 16:32:03 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, element) {
+  var newArray = [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array = [element, ...array]
+}
